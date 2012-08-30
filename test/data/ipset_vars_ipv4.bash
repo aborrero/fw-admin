035334578e39ced7faf6e9986124747928d9f4e3
Arturo Borrero Gonzalez <cer.inet@gmail.com>
2012-08-30 19:14:57 +0200

    mid: Fixed fw-admin, now correctly call to validate_execution(). added test dir for as a testbench place.

---
+++ test/data/ipset_vars_ipv4.bash
@@ -0,0 +1,6 @@
+IPSET=/usr/sbin/ipset
+inet=inet
+S_LDAP=s_ldap_ipv4
+S_RADIUS=s_radius_ipv4
+REP_RHEL=rep_rhel_ipv4
+S_DNS=s_dns_ipv4
---
+++ test/data/ipset_vars_ipv6.bash
@@ -0,0 +1,6 @@
+IPSET=/usr/sbin/ipset
+inet=inet6
+S_LDAP=s_ldap_ipv6
+S_RADIUS=s_radius_ipv6
+REP_RHEL=rep_rhel_ipv6
+S_DNS=s_dns_ipv6
