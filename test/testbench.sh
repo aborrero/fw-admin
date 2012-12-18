5861367e30e283ec2d45bf48cbaaf8e22e07aa2d
Arturo Borrero <aborrero@cica.es>
2012-12-18 13:56:46 +0100

    mid: added debug support. Added build tests.

--- test/testbench.sh
+++ test/testbench.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ `id -u` -ne 0 ]
 then
@@ -6,26 +6,47 @@ then
 	exit 0
 fi
 
-debug=">/dev/null 2>&1"
-if [ "$1" != "-d" ]
-then
-	debug=""
-	echo "I: Use '-d' to set debug mode."
-fi
-
-echo "##################################################################" >&2
-echo "W: Runing this testbench will break your current fw-admin system!!" >&2
+echo "##################################################################"
+echo "W: Runing this testbench will break your current fw-admin system!!"
 read -p "Continue? [N/y] " continue
 if [ -z "$continue" ] || [ $continue != y ]
 then
 	exit 0
 fi
 
+if [ "$1" != "-d" ]
+then
+	echo "I: Use '-d' to set debug mode."
+	exec 2>/dev/null
+fi
+
 fail=0
 
 ###########################################################
 ###########################################################
+echo "I: Testing building"
+echo -n "."
+cd ../src
+make >&2
+if [ $? -ne 0 ]
+then
+	echo ""
+	echo "E: Error building tar.gz package."
+	exit 1
+fi
+echo -n "."
+cd ../debian
+make >&2
+if [ $? -ne 0 ]
+then
+	echo ""
+	echo "E: Error building deb package."
+	exit 1
+fi
 
+###########################################################
+###########################################################
+echo ""
 echo "I: Testing datafiles"
 VALID="r2d2.cica.es www.google.es www.facebook.com github.com 2a00:9ac0:c1ca:27::150 150.214.4.150 192.168.1.1 fe00::1 ::1 127.0.0.1"
 INVALID="132.1244.123.21 dd::DDD:DDD::Dddd asdasd.asd.asd.asd.asd..asd"
@@ -33,13 +54,13 @@ INVALID="132.1244.123.21 dd::DDD:DDD::Dddd asdasd.asd.asd.asd.asd..asd"
 for i in $VALID
 do
 	echo -n "."
-	fw-admin -a $i $debug || fail=1
-	fw-admin -i $i $debug || fail=1
-	fw-admin -d $i $debug <<END
+	fw-admin -a $i 1>&2 || fail=1
+	fw-admin -i $i 1>&2 || fail=1
+	fw-admin -d $i 1>&2 <<END
 y
 END
 	[ $? -ne 0 ] && fail=1
-	fw-admin -a $i $debug || fail=1
+	fw-admin -a $i 1>&2 || fail=1
 
 done
 
@@ -47,8 +68,8 @@ done
 for i in $INVALID
 do
 	echo -n "."
-	fw-admin -a $i $debug && fail=1
-	fw-admin -i $i $debug && fail=1
+	fw-admin -a $i 1>&2 && fail=1
+	fw-admin -i $i 1>&2 && fail=1
 done
 
 if [ $fail -ne 0 ]
@@ -59,23 +80,21 @@ then
 fi
 
 # Sets
-SETS="test1 t-est2 te_st3 te.st4 te@st5"
+SETS="test1 test2 test3 test4 test5"
 for i in $SETS
 do
 	echo -n "."
-	fw-admin -as $i $debug || fail=1
-	fw-admin -i $i $debug || fail=1
-	fw-admin -d $i $debug <<END
+	fw-admin -d $i 1>&2 <<END
 y
 END
-	[ $? -ne 0 ] && fail=1
-	fw-admin -as $i $debug || fail=1
+	fw-admin -as $i 1>&2 || fail=1
+	fw-admin -i $i 1>&2 || fail=1
 done
 
 if [ $fail -ne 0 ]
 then
 	echo ""
-	echo "E: Errors found adding IPSET variables to datafiles."
+	echo "E: Errors found teste adding/deleting IPSET variables to datafiles."
 	exit 1
 fi
 
@@ -102,9 +121,9 @@ fi
 echo ""
 echo "I: Testing operations"
 echo -n "."
-fw-admin --start /dev/null $debug && fail=1
+fw-admin --start /dev/null 1>&2 && fail=1
 echo -n "."
-fw-admin --stop /dev/null $debug && fail=1
+fw-admin --stop /dev/null 1>&2 && fail=1
 
 if [ $fail -ne 0 ]
 then
