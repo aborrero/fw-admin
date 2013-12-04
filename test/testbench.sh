c838a3b412258e3e9389d1723ae6efe16c19ea12
Christian Mora <christian2201@gmail.com>
2013-12-04 13:02:22 +0100

    Add reporting error of testbench

--- test/testbench.sh
+++ test/testbench.sh
@@ -68,21 +68,21 @@ VALID="www.google.es www.facebook.com github.com 2a00:9ac0:c1ca:27::150 150.214.
 for i in $VALID
 do
 	echo -n "."
-	fw-admin -a $i 1>&2 || { fail=1 ; echo "*!*" ; }
-	fw-admin -i $i 1>&2 || { fail=1 ; echo "*!*" ; }
+	fw-admin -a $i 1>&2 || { fail=1 ; echo "" ; echo "*Error add variable*" ; }
+	fw-admin -i $i 1>&2 || { fail=1 ; echo "" ; echo "*Error search varible*" ; }
 	fw-admin -d $i 1>&2 <<END
 y
 END
-	[ $? -ne 0 ] && { fail=1 ; echo "*!*" ; }
-	fw-admin -a $i 1>&2 || { fail=1 ; echo "*!*" ; }
+	[ $? -ne 0 ] && { fail=1 ; echo "" ; echo "*fw-admin -d fail*" ; } 
+	fw-admin -a $i 1>&2 || { fail=1 ; echo "" ; echo "*Error add variable*" ; }
 
 done
 
 echo -n "."
-fw-admin --check-datafiles >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin --check-datafiles >&2 || { fail=1 ; echo "" ; echo "*Error check datafiles*" ; }
 
 echo "."
-fw-admin -r || { fail=1 ; echo "*!*" ; }
+fw-admin -r || { fail=1 ; echo "*Error reload variables*" ; }
 
 if [ $fail -ne 0 ]
 then
@@ -98,9 +98,9 @@ fi
 echo ""
 echo "I: Testing stats"
 echo -n "."
-fw-admin -s | grep  "Declared variables:" >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin -s | grep  "Declared variables:" >&2 || { fail=1 ; echo "" ; echo "*Error fw-admin -s (Not show varibles)*" ; }
 echo -n "."
-fw-admin -s | egrep "\[fw up\]"\|"\[fw down\]" >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin -s | egrep "\[fw up\]"\|"\[fw down\]" >&2 || { fail=1 ; echo "" ; echo "*Error fw-admin -s (Not show fw up | fw down)*" ; }
 
 if [ $fail -ne 0 ]
 then
@@ -120,60 +120,60 @@ cp -f data/* /var/lib/fw-admin/
 cp -f rules/* /etc/fw-admin.d/rules/
 
 echo -n "."
-fw-admin --start core >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin --start core >&2 || { fail=1 ; echo "" ; echo "*Init or reload rules fail*" ; }
 echo -n "."
-fw-admin -s | grep "\[fw up\]" >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin -s | grep "\[fw up\]" >&2 || { fail=1 ; echo "" ; echo "*Not show fw up on fw-admin -s*" ; }
 echo -n "."
-fw-admin --ipset-reload >&2 && { fail=1 ; echo "*!*" ; }
+fw-admin --ipset-reload >&2 && { fail=1 ; echo "" ; echo "*This command not should run*" ; }
 echo -n "."
-fw-admin --stop core >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin --stop core >&2 || { fail=1 ; echo "" ; echo "*Stop rules fail*" ; }
 echo -n "."
-fw-admin --start ./rules/core >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin --start ./rules/core >&2 || { fail=1 ; echo "" ; echo "*Fail start ./rules/core*" ; }
 echo -n "."
-fw-admin --start ./rules/vlan_1 >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin --start ./rules/vlan_1 >&2 || { fail=1 ; echo "" ; echo "*Fail start ./rules/vlan_1*" ; }
 echo -n "."
-fw-admin --start ./rules/sets >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin --start ./rules/sets >&2 || { fail=1 ; echo "" ; echo "*Fail start ./rules/sets*" ; }
 
 echo -n "."
-fw-admin --start vlan_2 >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin --start vlan_2 >&2 || { fail=1 ; echo "" ; echo "*Fail start vlan_2*" ; }
 
 echo -n "."
-fw-admin --start vlan_1 >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin --start vlan_1 >&2 || { fail=1 ; echo "" ; echo "*Fail start vlan_1*" ; }
 
 # void1 must appear in both
 echo -n "."
-iptables-save | grep void1 >&2 || { fail=1 ; echo "*!*" ; }
+iptables-save | grep void1 >&2 || { fail=1 ; echo "" ; echo "*Rule creating ipv4 fail 1*" ; }
 echo -n "."
-ip6tables-save | grep void1 >&2 || { fail=1 ; echo "*!*" ; }
+ip6tables-save | grep void1 >&2 || { fail=1 ; echo "" ; echo "*Rule creating ipv6 fail 1*" ; }
 # void2 must appear just in ipv4
 echo -n "."
-iptables-save | grep void2 >&2 || { fail=1 ; echo "*!*" ; }
+iptables-save | grep void2 >&2 || { fail=1 ; echo "" ; echo "*Rule creating ipv4 fail 2*" ; }
 echo -n "."
-ip6tables-save | grep void2 >&2 && { fail=1 ; echo "*!*" ; }
+ip6tables-save | grep void2 >&2 && { fail=1 ; echo "" ; echo "*Rule creating ipv6 fail 2*" ; }
 # void3 must not appear
 echo -n "."
-iptables-save | grep void3 >&2 && { fail=1 ; echo "*!*" ; }
+iptables-save | grep void3 >&2 && { fail=1 ; echo "" ; echo "*Rule creating ipv4 fail 3*" ; }
 echo -n "."
-ip6tables-save | grep void3 >&2 && { fail=1 ; echo "*!*" ; }
+ip6tables-save | grep void3 >&2 && { fail=1 ; echo "" ; echo "*Rule creating ipv6 fail 3*" ; }
 # void4 must appear just in ipv6
 echo -n "."
-iptables-save | grep void4 >&2 && { fail=1 ; echo "*!*" ; }
+iptables-save | grep void4 >&2 && { fail=1 ; echo "" ; echo "*Rule creating ipv4 fail 4*" ; }
 echo -n "."
-ip6tables-save | grep void4 >&2 || { fail=1 ; echo "*!*" ; }
+ip6tables-save | grep void4 >&2 || { fail=1 ; echo "" ; echo "*Rule creating ipv6 fail 4*" ; }
 # void5 must not appear
 echo -n "."
-iptables-save | grep void5 >&2 && { fail=1 ; echo "*!*" ; }
+iptables-save | grep void5 >&2 && { fail=1 ; echo "" ; echo "*Rule creating ipv4 fail 5*" ; }
 echo -n "."
-ip6tables-save | grep void5 >&2 && { fail=1 ; echo "*!*" ; }
+ip6tables-save | grep void5 >&2 && { fail=1 ; echo "" ; echo "*Rule creating ipv6 fail 5*" ; }
 
 # Testing FORMAT=restore
 echo -n "."
 sed -i s/FORMAT=script/FORMAT=restore/ /etc/fw-admin.d/fw-admin.conf
-fw-admin --start ruleset >&2 || { fail=1 ; echo "*!*" ; }
+fw-admin --start ruleset >&2 || { fail=1 ; echo "" ; echo "*Reload rules fail*" ; }
 
 echo -n "."
 # In FORMAT=restore, a file must be provided
-fw-admin --start >&2 && { fail=1 ; echo "*!*" ; }
+fw-admin --start >&2 && { fail=1 ;  echo "" ; echo "*Command --start /void/ fail  *" ; }
 
 if [ $fail -ne 0 ]
 then
@@ -193,38 +193,38 @@ cp -f conf/default_fw /etc/default/fw
 
 # Whatever state, stop now. Should return 0
 /etc/init.d/fw stop >&2
-[ "$?" != "0" ] && { fail=1 : echo "*!*" ; }
+[ "$?" != "0" ] && { fail=1 ;  echo "" ; echo "*/etc/init.d/fw stop fail (stop now)*" ; }
 
 # stop when already stopped should return 0
 echo -n "."
 /etc/init.d/fw stop >&2
-[ "$?" != "0" ] && { fail=1 ; echo  "*!*" ; }
+[ "$?" != "0" ] && { fail=1 ; echo "" ;  echo  "*/etc/init.d/fw stop fail (Stopped)*" ; }
 
 # status -> stopped should return 3
 echo -n "."
 /etc/init.d/fw status >&2
-[ "$?" != "3" ] && { fail=1 ; echo  "*!*" ; }
+[ "$?" != "3" ] && { fail=1 ; echo "" ; echo  "*/etc/init.d/fw status fail (Stopped)*" ; }
 
 # bad argument should return 4
 echo -n "."
 /etc/init.d/fw asdasd >&2
-[ "$?" != "4" ] && { fail=1 ; echo  "*!*" ; }
+[ "$?" != "4" ] && { fail=1 ; echo "" ; echo  "*Bad argument return fail*" ; }
 
 # success start should return 0
 echo -n "."
 /etc/init.d/fw start >&2
-[ "$?" != "0" ] && { fail=1 ; echo  "*!*" ; }
+[ "$?" != "0" ] && { fail=1 ; echo "" ; echo  "*/etc/init.d/fw start fail*" ; }
 
 # status -> started should return 0
 echo -n "."
 /etc/init.d/fw status >&2
-[ "$?" != "0" ] && { fail=1 ; echo  "*!*" ; }
+[ "$?" != "0" ] && { fail=1 ; echo "" ; echo  "*/etc/init.d/fw status fail (Started)/" ; }
 
 # if ENABLED=no, return code should be 3
 echo -n "."
 sed -i s/ENABLED=yes/ENABLED=no/ /etc/default/fw
 /etc/init.d/fw status >&2
-[ "$?" != "3" ] && { fail=1 ; echo  "*!*" ; }
+[ "$?" != "3" ] && { fail=1 ; echo "" ; echo  "*if ENABLED=no $?!=3*" ; }
 
 sed -i s/ENABLED=no/ENABLED=yes/ /etc/default/fw
 
@@ -234,7 +234,8 @@ sed -i s/PRE_STOP=\"\"// /etc/default/fw
 echo "$INST" >> /etc/default/fw
 if ! grep "$INST" /etc/default/fw >&2 ; then
 	fail=1
-	echo "*!*"
+	echo "" 
+	echo "*PRE_STOP fail*"
 else
 	echo -n "."
 fi
@@ -244,7 +245,8 @@ sed -i s/POST_STOP=\"\"// /etc/default/fw
 echo "$INST" >> /etc/default/fw
 if ! grep "$INST" /etc/default/fw >&2 ; then
 	fail=1
-	echo "*!*"
+	echo "" 
+	echo "*POST_STOP fail*"
 else
 	echo -n "."
 fi
@@ -256,28 +258,32 @@ echo -n "."
 if grep "pre_stop1" $TMPFILE >&2 ; then
 	echo -n "."
 else
-	echo "*!*"
+	echo "" 
+	echo "*pre_stop1 fail*"
 	fail=1
 fi
 
 if grep "pre_stop2" $TMPFILE >&2 ; then
 	echo -n "."
 else
-	echo "*!*"
+	echo "" 
+	echo "*pre_stop2 fail*"
 	fail=1
 fi
 
 if grep "post_stop1" $TMPFILE >&2 ; then
 	echo -n "."
 else
-	echo "*!*"
+	echo "" 
+	echo "*pre_stop1 fail*"
 	fail=1
 fi
 
 if grep "post_stop2" $TMPFILE >&2 ; then
 	echo -n "."
 else
-	echo "*!*"
+	echo ""
+	echo "*post_stop2 fail*"
 	fail=1
 fi
 
@@ -286,7 +292,8 @@ sed -i s/PRE_START=\"\"// /etc/default/fw
 echo "$INST" >> /etc/default/fw
 if ! grep "$INST" /etc/default/fw >&2 ; then
 	fail=1
-	echo "*!*"
+	echo "" 
+	echo "*PRE_START fail*"
 else
 	echo -n "."
 fi
@@ -297,7 +304,8 @@ sed -i s/POST_START=\"\"// /etc/default/fw
 echo "$INST" >> /etc/default/fw
 if ! grep "$INST" /etc/default/fw >&2 ; then
 	fail=1
-	echo "*!*"
+	echo "" 
+	echo "*POST_START fail*"
 else
 	echo -n "."
 fi
@@ -309,28 +317,32 @@ echo -n "."
 if grep "pre_start1" $TMPFILE >&2 ; then
 	echo -n "."
 else
-	echo "*!*"
+	echo "" 
+	echo "*fail pre_start1*"
 	fail=1
 fi
 
 if grep "pre_start2" $TMPFILE >&2 ; then
 	echo -n "."
 else
-	echo "*!*"
+	echo "" 
+	echo "*fail pre_start2*"
 	fail=1
 fi
 
 if grep "post_start1" $TMPFILE >&2 ; then
 	echo -n "."
 else
-	echo "*!*"
+	echo "" 
+	echo "*fail post_start1*"
 	fail=1
 fi
 
 if grep "post_start2" $TMPFILE >&2 ; then
 	echo -n "."
 else
-	echo "*!*"
+	echo "" 
+	echo "*fail post_start2*"
 	fail=1
 fi
 
